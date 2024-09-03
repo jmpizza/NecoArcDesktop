d9572426f2db2ec7f4160921d5701cbae5ed5be6
jmpizza <jespitiap@unal.edu.co>
2024-09-02 19:12:14 -0500

    Change of library from tkinter to pygame

--- NecoPet.py
+++ NecoPet.py
@@ -1,45 +1,42 @@
-import tkinter as tk
-import pyautogui as pyt
+import random as rd
+from screeninfo import get_monitors
+from PIL import Image
+import win32gui
+import win32con
+import win32api
+import pygame
 
 class Neco:
 
-    def __init__(self, path, screenHeight):
-        self.root = tk.Tk()
-        self.root.title('Neco')
-        self.root.geometry(f'+500+{screenHeight-215}')
-        self.root.config(highlightbackground='white')
-        self.root.overrideredirect(True)
-        self.root.wm_attributes('-transparentcolor', 'white', '-topmost', 1)
-        self.frames = [tk.PhotoImage(file=path, format=f"gif -index {i}").subsample(3,3) for i in range(10)]
-        self.label = tk.Label(self.root, image=self.frames[0],bd=0, bg='white')
-        self.label.pack()
-        self.index = 0
-    
-    def walkRight(self, times=10):
-        if times > 0:
-            self.label.config(image=self.frames[self.index % len(self.frames)])
-            self.root.update()
-            self.root.geometry(f'+{self.root.winfo_x()+10}+{self.root.winfo_y()}')
-            self.index += 1
-            if self.index >= 10: 
-                self.index = 0
-                times -= 1
-            self.root.after(100, self.walkRight, times)  
+    def __init__(self):
+        self.running = True
+        self.monitors = get_monitors()
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.monitors[0].width, self.monitors[0].height), pygame.NOFRAME | pygame.SRCALPHA)
+        self.clock = pygame.time.Clock()
 
-    def walkLeft(self, times=10):
-        if times > 0:
-            self.label.config(image=self.frames[self.index % len(self.frames)])
-            self.root.update()
-            self.root.geometry(f'+{self.root.winfo_x()-10}+{self.root.winfo_y()}')
-            self.index += 1
-            if self.index >= 10:
-                self.index = 0
-                times -= 1
-            self.root.after(100, self.walkRight, times)
+        self.green = (0, 255, 0)
 
-    def startMovement(self):
-        self.root.after(0,self.walkLeft)
-    
-    def run(self):
-        self.startMovement()
-        self.root.mainloop()
+        a = pygame.display.get_wm_info()['window']
+        win32gui.SetWindowLong(a, win32con.GWL_EXSTYLE,
+                            win32gui.GetWindowLong(a, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+        win32gui.SetLayeredWindowAttributes(a, win32api.RGB(*self.green), 0, win32con.LWA_COLORKEY)
+        win32gui.SetWindowPos(a, win32con.HWND_TOPMOST, 0, 0, 0, 0,
+                            win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
+        self.frame_index = 0
+
+
+    def update(self):
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+        self.screen.fill(self.green)
+
+
+    def walk(self, gif):
+        for i in range(len(gif)):
+            self.screen.fill(self.green)
+            self.screen.blit(gif[i], (0, 0))
+            pygame.display.flip()
+            self.clock.tick(9)
+        
--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import random as rd
-import tkinter as tk
-import pyautogui as pyt
+from screeninfo import get_monitors
 from NecoPet import Neco
+from PIL import Image
+import win32gui
+import win32con
+import win32api
+import pygame
 
-neco_walk = "gifs/Neco_Walk.gif"
+def extract_frames(numOfFrames, path):
+    gif = Image.open(path)
+    frames = []
+    for i in range(numOfFrames):
+        pil_image = gif.convert('RGBA')
+        frame = pygame.image.fromstring(pil_image.tobytes(), pil_image.size, 'RGBA')
+        frames.append(frame)
+        gif.seek(gif.tell() + 1)
+    return frames
+
+neco_walk = extract_frames(9,"gifs/Neco_Walk.gif")
 neco_angry = ''
 neco_run = ''
-screenWidth, screenHeight = pyt.size()
 
 if __name__ == "__main__":
-    neco = Neco(neco_walk, screenHeight)
-    neco.run()
+    neco = Neco()
+    while neco.running:
+        neco.update()
+        neco.walk(neco_walk)
+    pygame.quit()
+
